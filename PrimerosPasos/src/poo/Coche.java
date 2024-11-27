e87adee5d5bd9d875b7e7218368c32e197d8c99c
JaimeBonBol <jaimebb2002@gmail.com>
2024-11-27 18:29:09 +0100

    Continuación repaso java, poo

--- PrimerosPasos/src/poo/Coche.java
+++ PrimerosPasos/src/poo/Coche.java
@@ -1,18 +1,19 @@
 package poo;
 
 public class Coche {
+    //Las propiedades están encapusuladas, lo que hace que no se pueda modificarlos si no es desde un metodo de la propia clase.
     //Atributos o propiedades.
     private int ruedas;
     private int largo;
     private int ancho;
     private int motor;
     private int peso_plataforma;
-    //Estos atributo spueden variar dependiendo del coche.
-    String color;
-    int peso_total;
-    boolean asientos_cuero, climatizador;
+    //Estos atributos pueden variar dependiendo del coche.
+    private String color;
+    private int peso_total;
+    private boolean asientos_cuero, climatizador;
 
-    //Metodo constructor.
+    //Metodo constructor. Este metodo lo que hace es inicializar los atributos de la clase
      public Coche(){
          ruedas = 4;
          largo = 2000;
@@ -21,15 +22,72 @@ public class Coche {
          peso_plataforma = 500;
      }
 
-     public String getLargo(){
-         return "El largo del coche es "+largo;
+     public String getDatosGenerales(){
+         return "La plataforma del vehículo tiene "+ruedas+" ruedas. Mide " + largo/1000+" metros con un ancho de "+ancho+" cm y un peso de plataforma de "+
+                 peso_plataforma+ " kg";
      }
     //En principio vamos a poner que todos son de color azul.
-     public void setColor(){
-         color = "azul";
+     public void setColor(String color_coche){
+         this.color = color_coche;
      }
 
      public String getColor(){
          return "El color del coche es "+color;
+     }
+
+     public void setAsientos_cuero(String asientos_cuero){  //SETTER
+         if (asientos_cuero.equalsIgnoreCase("si")){
+             this.asientos_cuero = true;
+         }else {
+             this.asientos_cuero = false;
+         }
+     }
+
+     public String getAsientos_cuero(){
+         if (asientos_cuero==true){
+             return "El coche tiene asientos de cuero.";
+         }else {
+             return "El coche no tiene asientos de cuero.";
+         }
+     }
+
+     public void setClimatizador(String climatizador){
+         if (climatizador.equalsIgnoreCase("si")){
+             this.climatizador = true;
+         }else {
+             this.climatizador = false;
+         }
+     }
+
+     public String getClimatizador(){
+         if (climatizador==true){
+             return "El coche incorpora climatizador";
+         }else {
+             return "El coche no incorpora climatizador.";
+         }
+     }
+
+     public String dimePesoTotal(){      //SETTER + GETTER
+         int peso_carroceria = 500;
+         peso_total = peso_plataforma + peso_carroceria;
+
+         if (asientos_cuero == true){
+             peso_total = peso_total+50;
+         }
+         if (climatizador == true) {
+             peso_total = peso_total + 20;
+         }
+         return "El peso del coche es "+peso_total;
+     }
+
+     public int precioCoche(){
+         int precio_final = 10000;
+         if (asientos_cuero==true){
+             precio_final += 2000;
+         }
+         if (climatizador==true){
+             precio_final +=1500;
+         }
+         return precio_final;
      }
 }
--- PrimerosPasos/src/poo/Uso_Coche.java
+++ PrimerosPasos/src/poo/Uso_Coche.java
@@ -1,11 +1,23 @@
 package poo;
+import javax.swing.*;
 
 public class Uso_Coche {
     public static void main(String[] args) {
         Coche micoche = new Coche();
 
-        micoche.setColor();
+        micoche.setColor(JOptionPane.showInputDialog("Introduce color "));
+
+        System.out.println(micoche.getDatosGenerales());
+
         System.out.println(micoche.getColor());
-        System.out.println(micoche.getLargo());
+
+        micoche.setAsientos_cuero(JOptionPane.showInputDialog("¿Tiene asientos de cuero? "));
+        System.out.println(micoche.getAsientos_cuero());
+
+        micoche.setClimatizador(JOptionPane.showInputDialog("¿Tiene climatizador?"));
+        System.out.println(micoche.getClimatizador());
+
+        System.out.println(micoche.dimePesoTotal());
+        System.out.println("El precio final del coche es: "+micoche.precioCoche());
     }
 }
