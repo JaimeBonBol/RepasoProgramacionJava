d4ccf466153c6ed33c7f5b90ae30136ab89da1c5
JaimeBonBol <jaimebb2002@gmail.com>
2024-12-27 17:54:10 +0100

    Aplicaciones gráficas Swing II, colocación del tamaño y posición del frame

--- PrimerosPasos/src/Graficos/Marco.java
+++ PrimerosPasos/src/Graficos/Marco.java
@@ -1,11 +1,21 @@
 package Graficos;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Marco extends JFrame{
 
     public Marco(){
-        setSize(500,300);
+        //setSize(500,300);
+        //setLocation(500,300);
+
+        setBounds(500,300,550,250);
+
+        //setResizable(false);
+
+        //setExtendedState(Frame.MAXIMIZED_BOTH); //Para abrir el marco a pantalla conpleta.
+
+        setTitle("Mi ventana");
     }
 
 }
