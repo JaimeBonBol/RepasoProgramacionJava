2612435afabb78b2af6d0de4de510a4b484e4356
JaimeBonBol <jaimebb2002@gmail.com>
2024-12-28 16:28:06 +0100

    Aplicaciones gráficas Swing IV, escribiendo en el frame

---
+++ PrimerosPasos/src/Graficos/MarcoConTexto.java
@@ -0,0 +1,25 @@
+package Graficos;
+
+import javax.swing.*;
+
+public class MarcoConTexto extends JFrame {
+
+    public MarcoConTexto(){
+
+        setVisible(true);
+
+        setSize(600,450);
+
+        setLocation(400,200);
+
+        setTitle("Primer texto");
+
+        //Además de todas las características anteriores tiene que incluir una lamina de la clase Lamina creada antes.
+        Lamina miLamina = new Lamina();
+
+        //Para agregar lámina instanciada.
+        add(miLamina);
+
+    }
+
+}
