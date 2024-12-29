b93acbabd0486e7b42d61f57da1346a3e65662a4
JaimeBonBol <jaimebb2002@gmail.com>
2024-12-29 17:40:28 +0100

    Aplicaciones gráficas Swing IV, escribiendo en el Frame

---
+++ PrimerosPasos/src/Graficos/EscibiendoEnMarco/EscribiendoEnMarco.java
@@ -0,0 +1,15 @@
+package Graficos.EscibiendoEnMarco;
+
+import javax.swing.*;
+
+public class EscribiendoEnMarco {
+    public static void main(String[] args) {
+
+        MarcoConTexto miMarco = new MarcoConTexto();
+
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+}
---
+++ PrimerosPasos/src/Graficos/EscibiendoEnMarco/Lamina.java
@@ -0,0 +1,19 @@
+package Graficos.EscibiendoEnMarco;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Lamina extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        //LLama al metodo paintComponent de la clase JComponent para que haga su trabajo,
+        // y además de eso sibuja después un texto.
+
+        super.paintComponent(g);
+
+        g.drawString("Estamos aprendiendo Swing",100,100);
+
+    }
+}
