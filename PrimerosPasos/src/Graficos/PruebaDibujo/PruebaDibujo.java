b1ba56206c132bc87a6947728d4b4a9b941d765a
JaimeBonBol <jaimebb2002@gmail.com>
2024-12-29 17:43:58 +0100

    Aplicaciones gráficas Swing V, dibujando en el Frame

---
+++ PrimerosPasos/src/Graficos/PruebaDibujo/PruebaDibujo.java
@@ -0,0 +1,16 @@
+package Graficos.PruebaDibujo;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class PruebaDibujo {
+    public static void main(String[] args) {
+
+        MarcoConDibujos miMarco = new MarcoConDibujos();
+
+        miMarco.setVisible(true);
+
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
