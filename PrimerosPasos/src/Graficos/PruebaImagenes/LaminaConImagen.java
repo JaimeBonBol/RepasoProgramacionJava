040c33021de61897e68107c5d3715e4b73b2e892
JaimeBonBol <jaimebb2002@gmail.com>
2025-01-02 11:26:27 +0100

    Aplicaciones gráficas Swing IX y X, incluyendo imágenes

--- PrimerosPasos/src/Graficos/PruebaImagenes/LaminaConImagen.java
+++ PrimerosPasos/src/Graficos/PruebaImagenes/LaminaConImagen.java
@@ -10,11 +10,9 @@ public class LaminaConImagen extends JPanel {
 
     private Image imagen;
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public LaminaConImagen(){
 
-        File miImagen = new File("PrimerosPasos/src/Graficos/coche.png");
+        File miImagen = new File("PrimerosPasos/src/Graficos/bola.png");
 
         //Tratamiento de excpeciones.
         try {
@@ -23,7 +21,31 @@ public class LaminaConImagen extends JPanel {
             System.out.println("La imagen no se encuentra");
         }
 
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
         //g.drawImage(imagen,5,5,null);
-        g.drawImage(imagen,5,5,250,150,null);
+
+        //El operador this hace referencia al objeto dobde me encuentro, en este caso en la lámina, por lo tanto estoy informando
+        // de que es la lámina el objeto que espera que la imagen sea cargada.
+
+
+        g.drawImage(imagen,0,0,20,20,null);
+        int anchuraImagen = imagen.getWidth(this);
+        int alturaImagen = imagen.getHeight(this);
+        for (int i = 0; i < 300; i++) {
+            for (int j = 0; j < 200; j++) {
+                if (i+j>0){
+                    g.copyArea(0,0,anchuraImagen,alturaImagen,20*i,20*j);
+                }
+
+            }
+        }
+
+
     }
 }
