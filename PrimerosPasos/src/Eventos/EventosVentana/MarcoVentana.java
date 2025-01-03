69c7e84295f4a1a3e43b52d7bd5b5c84347a864f
JaimeBonBol <jaimebb2002@gmail.com>
2025-01-03 12:39:52 +0100

    Eventos IV y V, eventos de ventana I y II. Implementación de clases adaptadoras

--- PrimerosPasos/src/Eventos/EventosVentana/MarcoVentana.java
+++ PrimerosPasos/src/Eventos/EventosVentana/MarcoVentana.java
@@ -13,49 +13,24 @@ public class MarcoVentana extends JFrame {
 
         setVisible(true);
 
-        M_Ventana oyenteVentana = new M_Ventana();  //Creamos una instancia perteneciente a la clase oyente(Que es M_Ventana).
+        /*M_Ventana oyenteVentana = new M_Ventana();  //Creamos una instancia perteneciente a la clase oyente(Que es M_Ventana).
 
         //Ahora tenemos que poner el marco a la escucha, es decir que esté preparado para llamar a la clase oyente.
-        addWindowListener(oyenteVentana);
+        addWindowListener(oyenteVentana);*/
+
+        addWindowListener(new M_Ventana());
     }
 
 }
 
-class M_Ventana implements WindowListener{
-
-
-    @Override
-    public void windowOpened(WindowEvent e) {
-        System.out.println("Ventana abierta");
-    }
-
-    @Override
-    public void windowClosing(WindowEvent e) {
-        System.out.println("Cerrando ventana");
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-        System.out.println("La ventana ha sido cerrada");
-    }
+/**
+ * Como hereda de una clase adaptadora, ya no hace falta implementar todos los metodos, solo los que vayamos a usar.
+ */
+class M_Ventana extends WindowAdapter{
 
     @Override
     public void windowIconified(WindowEvent e) {
         System.out.println("Ventana minimizada");
     }
 
-    @Override
-    public void windowDeiconified(WindowEvent e) {
-        System.out.println("Ventana restaurada");
-    }
-
-    @Override
-    public void windowActivated(WindowEvent e) {
-        System.out.println("Ventana activada");
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) {
-        System.out.println("Ventana desactivada");
-    }
 }
