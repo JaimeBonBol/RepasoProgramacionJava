36dbc9e2b46f6ce2a899bc24648b778379aaf957
JaimeBonBol <jaimebb2002@gmail.com>
2025-01-16 13:42:13 +0100

    Continuación ej5 boletín 4 POO

--- EjerciciosPOO/src/POO4_EjerciciosPdfs/Ej5_Parking/Parking.java
+++ EjerciciosPOO/src/POO4_EjerciciosPdfs/Ej5_Parking/Parking.java
@@ -10,15 +10,29 @@ public class Parking {
     public Parking(String nombre, int numPlazas){
         this.nombre = nombre;
         this.matriculas = new ArrayList<>(numPlazas);
-        for (String plaza : matriculas){
+        for (int i = 0; i < numPlazas; i++) {
             matriculas.add(null);
         }
     }
+
+
+
+    public void getMatriculas() {
+        int contador = 1;
+        for (String plaza : matriculas){
+            System.out.println("Plaza "+contador+": "+plaza);
+            contador ++;
+        }
+    }
     
+
 }
 
 class Main{
     public static void main(String[] args) {
         Parking parking = new Parking("Arabial",10);
+
+        parking.getMatriculas();
+
     }
 }
