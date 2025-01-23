88b880ba0dfc8f98f2a8f47b610757a89be1002c
JaimeBonBol <jaimebb2002@gmail.com>
2025-01-23 10:42:26 +0100

    Continuación ejercicio 5 boletin 4

--- EjerciciosPOO/src/POO4_EjerciciosPdfs/Ej5_Parking/Parking.java
+++ EjerciciosPOO/src/POO4_EjerciciosPdfs/Ej5_Parking/Parking.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 
 public class Parking {
 
+    /**
+     * Atributos de la clase Parking
+     */
     protected ArrayList<String> matriculas;
     private String nombre;
 
+    /**
+     * Constructor de la clase parking
+     * @param nombre
+     * @param numPlazas
+     */
     public Parking(String nombre, int numPlazas){
         this.nombre = nombre;
         this.matriculas = new ArrayList<>(numPlazas);
@@ -15,7 +23,13 @@ public class Parking {
         }
     }
 
-
+    /**
+     * Metodo getter para obtener el valor de la variable nombre.
+     * @return
+     */
+    public String getNombre() {
+        return nombre;
+    }
 
     public void getMatriculas() {
         int contador = 1;
@@ -24,14 +38,28 @@ public class Parking {
             contador ++;
         }
     }
-    
+
+    public void entrada(String matricula, int plaza){
+        if (matricula.length()<4 || matricula == null) {
+            throw new RuntimeException ("Matricula incorrecta");
+        } else if (matriculas.get(plaza) != null) {
+            throw new RuntimeException("Plaza ocupada");
+        } else if (matriculas.contains(matricula)) {
+            throw new RuntimeException("Matricula repetida");
+        }else {
+            matriculas.set(plaza-1,matricula);
+        }
+    }
 
 }
 
 class Main{
     public static void main(String[] args) {
-        Parking parking = new Parking("Arabial",10);
+        Parking parking = new Parking("Arabial",11);
 
+        parking.getMatriculas();
+        parking.entrada("1234JGY",2);
+        System.out.println();
         parking.getMatriculas();
 
     }
