b703979dba3e70fd6a96bdf71498cce5e9359224
JaimeBonBol <jaimebb2002@gmail.com>
2025-03-23 16:55:07 +0100

    Ejercisio 5 ficheros

---
+++ EjerciciosPOO/src/Ficheros_AlmacenamientoDatos/EjerciciosFicheros/Ej5_Ficheros.java
@@ -0,0 +1,47 @@
+package Ficheros_AlmacenamientoDatos.EjerciciosFicheros;
+
+/*
+ Ejercicio 5: Contar palabras en un archivo
+ Crea un programa que cuente el número total de palabras en el archivo `datos.txt` y muestre el resultado en la consola.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ej5_Ficheros {
+    public static void main(String[] args) {
+        /**
+         * Variable donde guardo la ruta del archivo
+         */
+        String rutaArchivo = "EjerciciosPOO/src/Ficheros_AlmacenamientoDatos/EjerciciosFicheros/datos.txt";
+        File archivo = new File(rutaArchivo);
+
+        try{
+            //Instancia de BuffereReader pero que dentro tiene una instancia de FileReader.
+            BufferedReader lector = new BufferedReader(new FileReader(rutaArchivo));
+
+            // variable donde almaceno la línea leida y la voy mostrando.
+            String linea;
+
+            //Variable para llevar la cuenta de las palabras
+            int cuentaPalabras = 0;
+
+            // Se va repitiendo mientras que no devuleva un null que siginifica que ha llegado al final de la línea.
+            //Creo un array para guardar la línea dividida por cada espacio, y luego recorro ese array para contar cuantas palabras hay por línea.
+            while ((linea = lector.readLine()) != null){
+                String[] palabrasPorLinea = linea.split(" ");
+                for (String palabra : palabrasPorLinea){
+                    cuentaPalabras ++;
+                }
+            }
+
+            System.out.println("En el documento "+archivo.getName()+" hay un total de "+cuentaPalabras+" palabras.");
+            lector.close();
+
+        }catch (IOException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
