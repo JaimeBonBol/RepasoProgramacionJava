52336f84528644eef6ece5bfc34d2e7b1b9ff4d1
JaimeBonBol <jaimebb2002@gmail.com>
2025-03-25 12:42:54 +0100

    Ejercicio 12 ficheros

---
+++ EjerciciosPOO/src/Ficheros_AlmacenamientoDatos/EjerciciosFicheros/Ej12_Ficheros.java
@@ -0,0 +1,34 @@
+package Ficheros_AlmacenamientoDatos.EjerciciosFicheros;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ej12_Ficheros {
+    public static void main(String[] args) {
+        String rutaArchivo = "EjerciciosPOO/src/Ficheros_AlmacenamientoDatos/EjerciciosFicheros/datos.csv";
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(rutaArchivo));
+            File archivo = new File(rutaArchivo);
+
+            String linea;
+            System.out.println("Contenido del CSV "+archivo.getName()+": ");
+            while ((linea = lector.readLine()) != null){
+                String[] texto = linea.split(",");
+
+                for (String palabra : texto){
+                    if (palabra.isEmpty()){
+                        System.out.printf("               ");
+                    }else{
+                        System.out.printf("%-15s", palabra);
+
+                    }
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
